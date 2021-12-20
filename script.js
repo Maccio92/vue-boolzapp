103e91e1290bfe3f763d52b68a7670bf5b197580
Maccio92 <valerio362@gmail.com>
2021-12-20 15:53:40 +0100

    add vuejs

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto */ 
 
+let app = new Vue(
+    {
+    el: '#app',
+    data:{
 contacts: [
     {
       name: "Michele",
@@ -87,4 +91,7 @@ contacts: [
         },
       ],
     },
-  ];
+  ]
+}
+}
+  )
