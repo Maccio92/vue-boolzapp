52f9c409b002da812ba2f304b03c05952ac68a95
Maccio92 <valerio362@gmail.com>
2022-01-03 15:45:06 +0100

    add milestone 3

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ Milestone 2
 Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 Click sul contatto mostra la conversazione del contatto cliccato */
 
+/* Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+Milestone 5
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato */
+
 let app = new Vue(
     {
     el: '#app',
     data:{
       contatore: 0,
+      newMessage: '',
 contacts: [
     {
       name: "Michele",
@@ -93,14 +102,10 @@ contacts: [
           text: "Si, ma preferirei andare al cinema",
           status: "received",
         },
-        // {
-        //   date: "10/01/2020 16:50:00",
-        //   text: "Va bene, andiamo al cinema",
-        //   status: "received",
-        // }
       ],
     },
   ],
+  
 },
 methods: {
   change: function (index) {
@@ -108,7 +113,32 @@ methods: {
     console.log(this.contatore);
     
   },
-  }
-}
+  msg: function () {
+    // Aggiunta della data
+    dayjs.extend(window.dayjs_plugin_customParseFormat);
+    let data = dayjs().format("DD/MM/YYYY HH:mm:ss");
 
+    if (this.newMessage != ' ' && this.newMessage.length != 0){
+      this.contacts[this.contatore].messages.push({
+          text: this.newMessage,
+          date: data,
+          status: "sent"
+      });
+  }
+
+  setTimeout(() => {
+    this.contacts[this.contatore].messages.push({
+        text: "Ok",
+        date: data,
+        status: "received"
+    });
+}, 1000);
+  this.newMessage = '';
+  },
+    
+}}
   )
+  
+  
+
+
